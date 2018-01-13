304201bd2a9a0650b1d275a4fd1fa0578be38143
ZenZeno <zacherycharlesk@gmail.com>
2018-01-13 14:42:54 -0500

    Implemented newStdDeck, partially implemented shuffle

--- main.go
+++ main.go
@@ -3,78 +3,82 @@ package main
 import (
   "fmt"
   "math/rand"
+  "strconv"
   "time"
 )
 
-const numCards = 52
+const NUM_CARDS = 52
 
 func main() {
-  stdDeck := [numCards]string{
-    "As",
-    "2s",
-    "3s",
-    "4s",
-    "5s",
-    "6s",
-    "7s",
-    "8s",
-    "9s",
-    "10s",
-    "Js",
-    "Qs",
-    "Ks",
-    "Ac",
-    "2c",
-    "3c",
-    "4c",
-    "5c",
-    "6c",
-    "7c",
-    "8c",
-    "9c",
-    "10c",
-    "Jc",
-    "Qc",
-    "Kc",
-    "Ad",
-    "2d",
-    "3d",
-    "4d",
-    "5d",
-    "6d",
-    "7d",
-    "8d",
-    "9d",
-    "10d",
-    "Jd",
-    "Qd",
-    "Kd",
-    "Ah",
-    "2h",
-    "3h",
-    "4h",
-    "5h",
-    "6h",
-    "7h",
-    "8h",
-    "9h",
-    "10h",
-    "Jh",
-    "Qh",
-    "Kh",
-  }
-
   //create and seed a psuedo-random number stream
   shuffler := rand.New(rand.NewSource(time.Now().UnixNano()))
 
-  permutation := shuffler.Perm(numCards-1)
+  permutation := shuffler.Perm(NUM_CARDS-1)
 
-  fmt.Println(permutation)
-  fmt.Println("\n")
-  fmt.Println(stdDeck)
+  stdDeck := newStdDeck()
+  shuffledDeck := shuffle(stdDeck, permutation)
+
+  fmt.Println(stdDeck, "\n")
+  fmt.Println(permutation, "\n")
+  fmt.Println(shuffledDeck, "\n")
+}
+
+func newStdDeck() [NUM_CARDS]string {
+    var card string
+    var deck [NUM_CARDS]string
+    var a, b int
+    var suit []string
+
+    //construct each suit j, 0 - 3 in a slice and build an ordered array of cards:
+    for j := 0; j < 4; j++ {
+      //compute slice endpoints for suit j:
+      a = 13 * j //start at index 0 and shift 13 cards every suit
+      b = a + 13
+
+      //construct a slice to reference the current suit in deck array:
+      suit = deck[a:b]
+      fmt.Printf("Slice %v: [%v, %v)\n", j, a, b)
+
+      for i := 1; i <= NUM_CARDS / 4; i++ { //i cards in each suit
+        //give face cards proper names:
+        switch i {
+        case 1:
+          card = "A"
+        case 11:
+          card = "J"
+        case 12:
+          card = "Q"
+        case 13:
+          card = "K"
+        default:
+          card = strconv.Itoa(i)
+        }
+
+        //add suit:
+        switch j {
+        case 0:
+          card += "s"
+        case 1:
+          card += "c"
+        case 2:
+          card += "h"
+        case 3:
+          card += "d"
+        }
+
+        //write card to appropriate place in deck via suit:
+        suit[i-1] = card
+      }
+    }
+
+    return deck
 }
 
 //TODO:implement shuffle function
-func shuffle(cards [numCards]string, permutation []int) int {
+func shuffle(cards [NUM_CARDS]string, permutation []int) [NUM_CARDS]string {
+  var shuffledDeck [NUM_CARDS]string
+  for i := 0; i < NUM_CARDS; i++ {
+  }
 
+  return shuffledDeck
 }
